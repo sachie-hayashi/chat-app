6ab253df36a453efb3ad885617a20a278748a8aa
Sachie Hayashi <shayashi0428@gmail.com>
2023-01-08 00:28:18 -0800

    chore: setup styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
+// import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
 import './App.scss';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      {/* <Route path="/" element={<Dashboard />} /> */}
+      <Route path="/" element={<Register />} />
     </Routes>
   );
 };
---
+++ src/pages/Register/Register.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+  return (
+    <div className="section">
+      <div className="container">
+        <h1>Sign up</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
