ac003a754c79b9e80b455cf00fe545de3158210c
Sachie Hayashi <shayashi0428@gmail.com>
2023-01-31 13:27:22 -0800

    chore: remove useMemo from import for SearchForm.js

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useMemo } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 import { db } from '../../firebase';
 import { useSelector } from 'react-redux';
